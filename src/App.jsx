3b52d5351c73bc1d222ba43d925c92d9132733f5
priyanshu tiwari <priyanshu108tiwari@gmail.com>
2024-04-05 15:28:15 +0530

    app modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Frown } from "lucide-react";
 import Cards from "./components/cards/Cards";
 import Footer from "./components/Footer";
 import Marquees from "./components/marquees/Marquees";
@@ -10,15 +11,28 @@ import LocomotiveScroll from "locomotive-scroll";
 function App() {
   const locomotiveScroll = new LocomotiveScroll();
   return (
-    <div className="bg-zinc-900  h-full font-sans w-full  text-white">
-      <Navbar />
-      <Work />
-      <Stripes />
-      <Products />
-      <Marquees />
-      <Cards />
-      <Footer />
-    </div>
+    <>
+      <div className="hidden md:block bg-zinc-900  h-full font-sans w-full  text-white">
+        <Navbar />
+        <Work />
+        <Stripes />
+        <Products />
+        <Marquees />
+        <Cards />
+        <Footer />
+      </div>
+
+      <div className=" md:hidden w-full h-screen  uppercase px-6  bg-zinc-900 flex flex-col gap-3 items-center justify-center text-zinc-300">
+        <p className="w-full text-center text-4xl font-light  font-Poppins">
+          This is not responsive. Try with
+          <span className="text-purple-500"> desktop view!</span>
+        </p>
+        <div className="flex gap-2">
+          <Frown className="w-20 h-20 text-zinc-300" />
+          <Frown className="w-20 h-20 text-zinc-300" />
+        </div>
+      </div>
+    </>
   );
 }
 
